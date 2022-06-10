3fcb2469ca1f40fc47d92d39969a188465f1b102
D3moniako <soleroboo@gmail.com>
2022-06-10 15:00:34 +0200

    Primo commit del progetto Spring

---
+++ citoproject/src/main/java/com/libreria/citoproject/CitoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.libreria.citoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitoprojectApplication.class, args);
+	}
+
+}
